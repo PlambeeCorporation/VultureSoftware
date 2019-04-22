64582ea8da7e8acd4b0395ba6758b2e15f94aa30
kotprzemyslaw <kotowiczprzemyslaw@protonmail.com>
2019-04-22 09:01:17 +0100

    Backup of jobview.

--- src/com/plambeeco/view/jobviews/partsview/PartDetailsViewController.java
+++ src/com/plambeeco/view/jobviews/partsview/PartDetailsViewController.java
@@ -1,4 +1,37 @@
 package com.plambeeco.view.jobviews.partsview;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
 public class PartDetailsViewController {
+    @FXML
+    private ComboBox<String> cbPartNames;
+    @FXML
+    private TextField txtQuantity;
+    
+    private Stage partDetailsStage;
+
+    public void setPartDetailsStage(Stage partDetailsStage) {
+        this.partDetailsStage = partDetailsStage;
+    }
+
+    @FXML
+    private void handleOK(){
+        if(isInputValid()){
+
+        }
+
+        partDetailsStage.close();
+    }
+
+    @FXML
+    private void handleCancel(){
+        partDetailsStage.close();
+    }
+
+    private boolean isInputValid() {
+        return true;
+    }
 }
